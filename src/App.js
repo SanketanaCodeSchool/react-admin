48235467dc15966cf699715cbdbd6de0c90c873e
e-shaan <eshaan.gupta101@gmail.com>
2022-03-22 08:34:22 +0530

    Routes Working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import Home from './pages/home/Home.jsx'
 
-function App() {
-  return (
-    <div className="App">
-      <Home/>
+import {BrowserRouter , Routes, Route} from 'react-router-dom'
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import List from './pages/list/List';
+import New from './pages/new/New';
+import Single from './pages/single/Single';
+
+
+function App(){
+  return(
+    <div className='App'>
+      <BrowserRouter>
+        <Routes>
+          <Route path = "/">
+            <Route index element = {<Home/>} />
+            <Route path = "login" element = {<Login/>} />
+            <Route path = "users">
+              <Route index element = {<List/>}/>
+              <Route path = ":userID" element = {<Single/>} />
+              <Route path = "new" element = {<New/>} />
+            </Route>
+            <Route path = "products">
+              <Route index element = {<List/>}/>
+              <Route path = ":productID" element = {<Single/>} />
+              <Route path = "new" element = {<New/>} />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {BrowserRouter , Routes, Route} from 'react-router-dom'
 
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path = "/">
-            
-      </Route>
-    </Routes>
-  </BrowserRouter>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
